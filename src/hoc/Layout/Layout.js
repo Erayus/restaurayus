23259be81beef33c47daf93d6cbded572baef810
Erayus <raymondhieutran@gmail.com>
2019-11-07 16:44:53 +1100

    [New] Added Layout HOC

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Layout extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div>Navigation Bar</div>
+                <div>Main Content</div>
+            </React.Fragment>
+        )
+    }
+}
+export default Layout;
